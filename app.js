e891d0f4ec0c92007e322ce8e3e8c19b5c8cb04b
khanshafhad <83412470+khanshafhad@users.noreply.github.com>
2022-05-29 11:58:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const sections =document.querySelectorAll('.section');
+const sectBtns =document.querySelectorAll('.controlls');
+const sectBtn =document.querySelectorAll('.control');
+const allSections =document.querySelector('.main-content');
+
+function PageTransisons()
+{
+   for(let i =0;i< sectBtn.length;i=i+1)
+   {
+       sectBtn[i].addEventListener('click',function()
+       {
+           let currentBtn = document.querySelectorAll('.active-btn');
+           currentBtn[0].className=currentBtn[0].className.replace('active-btn', '');
+          
+           this.className+='active-btn'
+       })
+   }
+
+   //section active classs 
+
+   allSections.addEventListener('click',(e)=>{
+      
+       const id =e.target.dataset.id;
+       if(id)
+       {
+           //remove selected from the others  btns 
+           sectBtn.forEach((btn)=>{
+               btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other section
+
+            sections.forEach((section) =>
+            {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active')
+       }
+   })
+
+
+}
+
+PageTransisons();
